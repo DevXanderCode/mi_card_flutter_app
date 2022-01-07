7032e6b9ac2c6477fa825dcd5b9beffb1af71d19
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-07 19:01:44 +0100

    added my name

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundColor: Colors.red,
-              backgroundImage: AssetImage("images/face.jpg"),
-            )
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage("images/face.jpg"),
+              ),
+              Text(
+                "Chinedu Alex",
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
