87dbcc06992c0cb6bf920426a01dd03c7b6054f2
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-07 18:34:16 +0100

    my implimentation of the layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      color: Colors.yellowAccent[100],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
